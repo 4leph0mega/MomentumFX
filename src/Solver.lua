ebe73b9ef81b40c855290e16494bfb5db05ff01d
4leph0mega <108088079+4leph0mega@users.noreply.github.com>
2025-06-24 19:28:31 -0600

    Update Solver.lua

--- src/Solver.lua
+++ src/Solver.lua
@@ -1,1 +1,13 @@
+local Solver = {}
+local Helper = require("Helper")
+
+function Solver.Step(Animation, DeltaTime, Time)
+    local Speed = Animation.EasingFunc(Animation.Goal, Animation.Current, Animation.Speed, DeltaTime, Animation.Params, Time)
+    Animation.Speed = Speed
+    Animation.Current = Animation.Current + Animation.Speed * DeltaTime
+    Animation.Current = Helper.Clamp(Animation.Current,Animation.BoundMin,Animation.BoundMax)
+    return Animation
+end
+
+return Solver
 
